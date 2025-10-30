0c85659c379e82c2d3c0ad00ddd90a89bcee0f5d
Gabrielee27 <gabri2004.gdd@gmail.com>
2025-10-30 11:44:23 +0100

    fix - Javaspring

---
+++ JAVA-SPRING/eclipse-workspace/CorsoJava/src/com/string/Concat.java
@@ -0,0 +1,15 @@
+package it.string;
+
+public class Concat {
+
+	public static void main(String[] args) {
+
+		String name = "fabio";
+		String surname = "di gloria";
+		String nameSurname = name.concat(surname);
+
+		System.out.println(nameSurname);
+
+	}
+
+}
---
+++ JAVA-SPRING/eclipse-workspace/CorsoJava/src/com/string/Length.java
@@ -0,0 +1,16 @@
+package it.string;
+
+public class length {
+
+    public static void main(String[] args) {
+
+        String oggetto = "bottiglia";
+        System.out.println(oggetto.lenght());
+        
+        
+       int lunghezza = oggetto,lenght();
+       System.out.println(lunghezza);
+       
+        
+    }
+}
